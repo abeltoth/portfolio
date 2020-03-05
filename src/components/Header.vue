f87aefe1ef410281c4378fffd20e6861587c3bae
Ábel Tóth <abel.toth@nuage.hu>
2020-03-05 23:17:39 +0100

    Header desktop mode

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="header">
     <div class="home-link-container">
-      <router-link to="/" class="link-item home-link" active-class="active"><img src="../assets/logo.png" alt="logo"></router-link>
+      <router-link to="/" class="link-item home-link" active-class="active"><img src="../assets/logo.png" alt="logo" @click="isMenuVisible=false"></router-link>
     </div>
-    <div class="menu-icon-container" @click="isMenuVisible = !isMenuVisible">
-      <i class="material-icons menu-icon">menu</i>
-    </div>
-    <transition name="fade">
-      <div v-if="isMenuVisible" class="links">
+    <!-- Desktop menu -->
+      <div v-if="!mobileMode" class="desktop-links">
         <router-link to="/about" class="link-item" active-class="active"><a>About</a></router-link>
         <router-link to="/skills" class="link-item" active-class="active"><a>Skills</a></router-link>
         <router-link to="/work-experience" class="link-item" active-class="active"><a>Work experience</a></router-link>
         <router-link to="/portfolio" class="link-item" active-class="active"><a>Portfolio</a></router-link>
         <router-link to="/contact" class="link-item" active-class="active"><a>Contact</a></router-link>
+      </div>
+    <!-- Mobile menu -->
+      <div v-if="mobileMode" class="menu-icon-container" @click="isMenuVisible = !isMenuVisible">
+        <i class="material-icons menu-icon">menu</i>
+      </div>
+    <transition name="translateY">
+      <div v-if="mobileMode && isMenuVisible" class="mobile-links">
+        <router-link to="/about" class="link-item" active-class="active"><a @click="isMenuVisible=false">About</a></router-link>
+        <router-link to="/skills" class="link-item" active-class="active"><a @click="isMenuVisible=false">Skills</a></router-link>
+        <router-link to="/work-experience" class="link-item" active-class="active"><a @click="isMenuVisible=false">Work experience</a></router-link>
+        <router-link to="/portfolio" class="link-item" active-class="active"><a @click="isMenuVisible=false">Portfolio</a></router-link>
+        <router-link to="/contact" class="link-item" active-class="active"><a @click="isMenuVisible=false">Contact</a></router-link>
       </div>
     </transition>
   </div>
@@ -23,17 +32,34 @@ export default {
   name: "navbar",
   data() {
     return {
-      isMenuVisible: false
+      isMenuVisible: false,
+      mobileMode: false
+    }
+  },
+  created() {
+    window.addEventListener('resize', this.handleResize);
+    this.handleResize();
+  },
+  destroyed() {
+    window.removeEventListener('resize', this.handleResize);
+  },
+  methods: {
+    handleResize() {
+      if (window.innerWidth < 768) {
+        this.mobileMode = true;
+      } else {
+        this.mobileMode = false;
+      }
     }
   }
 };
 </script>
 
 <style>
-  .fade-enter-active, .fade-leave-active {
+  .translateY-enter-active, .translateY-leave-active {
     transition: all 1s ease;
   }
-  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  .translateY-enter, .translateY-leave-to {
     transform: translateY(-420px);
   }
 
@@ -42,7 +68,7 @@ export default {
     grid-template-columns: 1fr;
     grid-template-rows: auto;
   }
-  .links {
+  .mobile-links {
     display: grid;
     grid-template-columns: 1fr;
     grid-template-rows: auto;
@@ -51,11 +77,17 @@ export default {
     top: 70px;
     width: 100%;
   }
+  .desktop-links {
+    display: grid;
+    grid-template-columns: repeat(5, auto);
+    grid-template-rows: auto;
+    background-color: #262626;
+  }
   .link-item {
     color: #a0a0a0;
     margin: auto;
     height: 70px;
-    font-size: 22px;
+    font-size: 20px;
     display: grid;
     align-content: center;
   }
@@ -90,5 +122,14 @@ export default {
     font-size: 36px;
     float: right;
     margin: 17px;
+  }
+
+  @media (min-width: 768px) {
+    .links {
+      grid-template-columns: repeat(5, auto);
+    }
+    .header {
+      grid-template-columns: 1fr 2fr;
+    }
   }
 </style>
